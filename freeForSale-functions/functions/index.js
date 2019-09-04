7715e0407853e1ab6ad2b0aa5ddce72fa114b0b6
Caitlin <clee1228@berkeley.edu>
2019-09-03 23:16:11 -0700

    set up/validate login/signup

--- freeForSale-functions/functions/index.js
+++ freeForSale-functions/functions/index.js
@@ -115,6 +115,17 @@ app.post(`/post`, (request, response) => {
     
 // });
 
+const isEmpty = (string) => {
+    //eliminate white spaces
+    if(string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
 
 //Signup route
 app.post(`/signup`, (req, res) => {
@@ -125,7 +136,21 @@ app.post(`/signup`, (req, res) => {
         username: req.body.username, 
     };
 
+    let errors = {};
+
     //validate data
+    if(isEmpty(newUser.email)){
+        errors.email = 'Must not be empty';
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address';
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if(newUser.password !== newUser.confirmPw) errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.username)) errors.username = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
     let token, userId;
     db.doc(`/users/${newUser.username}`).get()
         .then(doc => {
@@ -161,6 +186,35 @@ app.post(`/signup`, (req, res) => {
             }
         });
 });
+
+
+//log-in route
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    
+    let errors = {};
+    if(isEmpty(user.email)) errors.email = 'Must not be empty';
+    if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({token});
+        })
+        .catch((err) => {
+            console.error(err);
+            return res.status(500).json({ error: err.code});
+        });
+
+});
+
 
 //pass in app instead of one fn/route which automatically turns into multiple routes
 exports.api = functions.https.onRequest(app);
